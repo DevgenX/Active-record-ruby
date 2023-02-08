10503a97c0b447129b80f6818163bf6617f078b0
Seb <107775878+DevgenX@users.noreply.github.com>
2023-02-08 17:27:36 -0500

    Delete phase-3-mock-code-challenge-freebie-tracker directory

--- phase-3-mock-code-challenge-freebie-tracker/app/models/freebie.rb
+++
@@ -1,8 +0,0 @@
-class Freebie < ActiveRecord::Base
-    belongs_to :dev
-    belongs_to :company
-    
-    def print_details
-        "#{self.dev.name} owns a #{self.item_name} from #{self.company.name}"
-    end
-end
